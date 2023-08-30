0cb336ee1dd8f35d90a3e300fcc4fa3bdf9b0b9d
ShikharB92 <shikharbhatnagar18@gmail.com>
2023-08-31 02:00:02 +0530

    account commit minus test case

--- TaskManagementSystem/accounts/views.py
+++ TaskManagementSystem/accounts/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from rest_framework import generics
 
-# Create your views here.
+from .models import CustomUser
+
+from rest_framework.permissions import AllowAny, IsAdminUser
+
+from .serializers import (
+    UserListSerializer,
+    UserCreateSerializer
+)
+
+
+class UserListView(generics.ListCreateAPIView):
+    permission_classes = (IsAdminUser,)
+    queryset = CustomUser.objects.all()
+    serializer_class = UserListSerializer
+
+
+class UserCreate(generics.CreateAPIView):
+    model = get_user_model()
+    permission_classes = [AllowAny]
+    serializer_class = UserCreateSerializer
+
