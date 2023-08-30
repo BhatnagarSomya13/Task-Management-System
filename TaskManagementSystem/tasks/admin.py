bf4b2216e034ae2a2bcc4e753802f8f37971cd5b
ShikharB92 <shikharbhatnagar18@gmail.com>
2023-08-30 23:55:13 +0530

    tasks commit

--- TaskManagementSystem/tasks/admin.py
+++ TaskManagementSystem/tasks/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Task
+
+class TaskAdmin(admin.ModelAdmin):
+	list_filter = ('completed', 'created_at', 'user')
+	list_editable = ('completed',)
+	search_fields = ('title', 'description')
+	list_display = ['title', 'completed']
+
+	class Meta:
+		model = Task
+	
+admin.site.register(Task, TaskAdmin)
